fbc2d756453fc0eccf448878f24903988bc31cf2
Ovi Sofariu <ovi@sofariu.com>
2015-07-11 22:40:16 -0400

    immutable with conditionals

--- src/test/scala/katas/FizzBuzzSpec.scala
+++ src/test/scala/katas/FizzBuzzSpec.scala
@@ -4,9 +4,18 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
 
-   "fizz Buzz" should "return Fizz when appropriate for range" in {
-      val fb  = new FizzBuzz(1 to 3)
-      print(fb.fizzBuzz())
-      fb.fizzBuzz() should be (List("1", "2", "FIZZ"))
+  "fizz buzz" should "return Fizz every multiple of 3 for range 1 to 3" in {
+     val fb  = new FizzBuzz(1 to 3)
+     fb.fizzBuzz() should be (List("1", "2", "FIZZ"))
+  }
+
+  "fizz buzz" should "return Buzz every multiple of 5 for range 1 to 5" in {
+    val fb = new FizzBuzz(1 to 5)
+    fb.fizzBuzz() should be (List("1", "2", "FIZZ", "4", "BUZZ"))
+  }
+
+  "fizz buzz" should "return FizzBuzz every multiple of 3 and 5 for range 1 to 15" in {
+    val fb = new FizzBuzz(1 to 15)
+    fb.fizzBuzz() should be (List("1", "2", "FIZZ", "4", "BUZZ", "FIZZ", "7", "8", "FIZZ", "BUZZ", "11", "FIZZ", "13", "14", "FIZZBUZZ"))
   }
 }
