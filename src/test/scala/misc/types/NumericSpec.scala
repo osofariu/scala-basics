8cec488d3b49dbaa84e3b7829fe451291f0ae488
Ovi Sofariu <ovi@sofariu.com>
2016-01-16 22:34:49 -0500

    cleaned up NumericSpec

--- src/test/scala/misc/types/NumericSpec.scala
+++ src/test/scala/misc/types/NumericSpec.scala
@@ -4,55 +4,45 @@ import org.scalatest.{path, Matchers}
 
 class NumericSpec extends path.FunSpec with Matchers {
 
+  trait Numeric[T] {
+    def +[_](other: Numeric[_]): Numeric[_]
+  }
+
+  case class IntNumeric[_](value: Int) extends Numeric[Int] {
+    override def +[_](other: Numeric[_]): Numeric[_] = other match {
+      case n: IntNumeric[_] => IntNumeric(value + n.value)
+      case d: DoubleNumeric[_] => DoubleNumeric(value + d.value)
+    }
+  }
+
+  case class DoubleNumeric[_](value: Double) extends Numeric[Double] {
+    override def +[_](other: Numeric[_]): Numeric[_] = other match {
+      case n: IntNumeric[_] => DoubleNumeric(n.value + value)
+      case d: DoubleNumeric[_] => DoubleNumeric(value + d.value)
+    }
+  }
+
+  implicit def toInt(n: IntNumeric[_]): Int =  n.value
+  implicit def toDouble(n: DoubleNumeric[_]) : Double = n.value
+
   describe("Numeric allows related types to be combined") {
 
-    trait Numeric[T] {
-      def +[_](other: Numeric[_]): Numeric[_]
-      def get: T
-    }
-
-    object IntNumeric {
-      def apply(i: Int) = new IntNumeric(i)
-    }
-
-    object DoubleNumeric {
-      def apply(d: Double) = new DoubleNumeric(d)
-    }
-
-    class IntNumeric[_] (val value: Int) extends Numeric[Int] {
-      override def +[_](other: Numeric[_]): Numeric[_] = other match {
-        case n: IntNumeric[_] => IntNumeric(value + n.value)
-        case d: DoubleNumeric[_] => DoubleNumeric(value + d.value)
-      }
-
-      implicit override def get = value
-    }
-
-    class DoubleNumeric[_](val value: Double) extends Numeric[Double] {
-      override def +[_](other: Numeric[_]): Numeric[_] = other match {
-          case n: IntNumeric[_] =>  DoubleNumeric(n.value + value)
-          case d: DoubleNumeric[_] => DoubleNumeric(value + d.value)
-      }
-
-      implicit override def get = value
-    }
-
-
     it("adds int to int") {
-      val v  = IntNumeric(12) + IntNumeric(3)
-      v.get shouldBe 15
-      v.get.isInstanceOf[Int] shouldBe true
+      0 + IntNumeric(12) + IntNumeric(3) shouldBe 15
+    }
+
+    it("even converts to Int automatically") {
+      0 + IntNumeric(12) shouldBe 12
     }
 
     it("adds int to double") {
-      val v = IntNumeric(12) + DoubleNumeric(12.5)
-      v.get shouldBe 24.5
+      val v: Double = 0 + IntNumeric(12) + DoubleNumeric(12.5)
+      v shouldBe 24.5
     }
 
     it("adds double to double") {
-      val v = DoubleNumeric(.5) + DoubleNumeric(.5)
-      v.get shouldBe 1.0
-      v.get.isInstanceOf[Double] shouldBe true
+      val v: Double = 0 + DoubleNumeric(.5) + DoubleNumeric(.5)
+      v shouldBe 1.0
     }
   }
 }
